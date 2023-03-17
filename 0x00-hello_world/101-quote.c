b90e0404ddff70257f67f2b7e2ef9f876b17c861
mmwa4 <muyamwa@gmail.com>
2023-03-16 19:51:22 -0700

    new commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Starting point for the code that is about to be executed.
+* Return: The statement will always return 1 thus printing the statement
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
